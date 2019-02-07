b267abb6aae4f61deada34c7f157dff14dec9c64
sinahk <sinahk1992@yahoo.com>
2019-02-07 17:15:07 +0330

    init

---
+++ SillyApps/Controllers/SillyWorkoutTimerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SillyWorkoutTimerViewController.swift
+//  SillyApps
+//
+//  Created by sinahk on 2/6/19.
+//  Copyright © 2019 sinakhodadad. All rights reserved.
+//
+
+import UIKit
+
+class SillyWorkoutTimerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
